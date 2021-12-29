d0dd7263283579702e5fca77952570483b5c0b32
Farenn <80033281+Fachrenv89@users.noreply.github.com>
2021-12-29 15:44:36 +0700

    Add files via upload

---
+++ code 1/perkenalan.php
@@ -0,0 +1,16 @@
+<?php
+
+// untuk menampilkan Hallo Nama Saya Fachren
+echo "Hallo Nama Saya Fachren";
+
+// untuk jarak baris di atas dan di bawah 
+echo "<br>";
+
+// untuk menampilkan Panggilan Nama Saya Faren
+echo "Panggilan Nama Saya Faren";
+
+// untuk jarak baris di atas dan di bawah 
+echo "<br>";
+
+// untuk menampilkan Saya Berasal Dari Kota Pekanbaru, Provinsi Riau
+echo "Saya Berasal Dari Kota Pekanbaru, Provinsi Riau";
---
+++ code 1/perkenalan_html&php.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fachren</title>
+</head>
+
+<body>
+
+	<!-- heading yang digunakan untuk penulisan judul utama dari dokument. -->
+	<h1>
+
+		<?php
+		// untuk menampilkan Hallo Nama Saya Fachren
+		echo "Saya Adalah Fachren";
+		?>
+	</h1>
+
+	<?php
+	// untuk menampilkan Hallo Nama Saya Fachren
+	echo "
+		<ol type='1'>
+			<li>item list</li>
+			<li>item list</li>
+			<li>item list</li>
+		</ol>
+		<ol type='a'>
+			<li>item list</li>
+			<li>item list</li>
+			<li>item list</li>
+		</ol>
+		<ol type='A'>
+			<li>item list</li>
+			<li>item list</li>
+			<li>item list</li>
+		</ol>
+		<ol type='i'>
+			<li>item list</li>
+			<li>item list</li>
+			<li>item list</li>
+		</ol>
+		<ol type='I'>
+			<li>item list</li>
+			<li>item list</li>
+			<li>item list</li>
+		</ol>
+	";
+
+	?>
+</body>
+
+</html>
